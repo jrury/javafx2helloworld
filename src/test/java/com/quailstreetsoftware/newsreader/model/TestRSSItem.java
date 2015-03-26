0900839622403e8ffd4f6e3819d5a3f70d95810c
Jonathan Rury <bitbucket@jonathanrury.com>
2015-03-26 09:13:32 -0400

    POM to specify Java 1.8 and added a silly test class so the structure is there.

---
+++ src/test/java/com/quailstreetsoftware/newsreader/model/TestRSSItem.java
@@ -0,0 +1,14 @@
+package com.quailstreetsoftware.newsreader.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestRSSItem {
+
+	@Test
+	public void test() {
+		// come up with some tests.
+		Assert.assertTrue(true);
+	}
+
+}
